db49ab649f13f96671610448cdf45ce868d3756a
siggo1234 <156718665+siggo1234@users.noreply.github.com>
2025-06-05 01:48:13 +0200

    Update evaluation_singletry.py

--- evaluation_singletry.py
+++ evaluation_singletry.py
@@ -543,7 +543,8 @@ def evaluate_prolog_generation(
 
     for idx, sample in enumerate(tqdm(dataset, desc="Evaluating")):
         sample_index = idx + 1
-
+        has_reference = False
+        
         prompt_text = ""
         model_text = ""
         prolog_code = ""
